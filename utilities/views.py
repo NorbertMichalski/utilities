da27a23add9c42d62ae21a5e74eef920bbd3d839
NorbertMichalski <Norbertmichalski16@gmail.com>
2014-01-27 14:45:28 +0100

    Update views.py

--- utilities/views.py
+++ utilities/views.py
@@ -6,6 +6,7 @@ def home(request):
     page = ('<ul><li><a href="/utilities/supply">Supply Check</a></li>'
             '<li><a href="/utilities/tracking">Tracking Numbers</a></li>'
             '<li><a href="/utilities/shipping">Shipping Calculator</a></li>'
+            '<li><a href="/utilities/freightsolution">Freight Calculator</a></li>'
             '<li><a href="/utilities/shipping/international">International Shipping</a></li>'
             '<li><a href="/utilities/resale_cert">Resale Certifications</a></li>'
             '<li><a href="admin">Tutorials</a></li></ul>')
